423cf4577d01e9059e5f3f42dc1744d3ae169b49
Ori Nachum <ori.nachum@gmail.com>
2023-07-03 19:56:05 +0000

    test(redis): try different startup

--- examples/vector_databases/redis/setup.sh
+++ examples/vector_databases/redis/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "Setting up vector db"
 docker-compose up -d
 
+echo "Setting up python e"
 python3 -m venv /workspaces/openai-cookbook/examples/vector_databases/redis/.venv
 pip3 install -r requirements.txt
 
+echo "Preparing data"
 python3 gettingStartedSetup.py
