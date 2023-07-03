049f29c69f5bd884b2f62a3cf3c075164f57c162
Ori Nachum <ori.nachum@gmail.com>
2023-07-03 22:56:13 +0000

    fix(redis): add venv setup

--- examples/vector_databases/redis/startApi.sh
+++ examples/vector_databases/redis/startApi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ./setup.sh
 
+source /workspaces/openai-cookbook/examples/vector_databases/redis/.venv/bin/activate
+
 echo "Starting uvicorn"
 uvicorn app:app --reload --host 0.0.0.0 --port 8000
