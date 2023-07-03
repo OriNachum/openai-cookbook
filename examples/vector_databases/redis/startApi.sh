4d74e4e30ee02dc68cfe07b6a24071373b6680d5
Ori Nachum <ori.nachum@gmail.com>
2023-07-03 22:58:43 +0000

    fix(api): remove extra venv setup, add uvicorn loc

--- examples/vector_databases/redis/startApi.sh
+++ examples/vector_databases/redis/startApi.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ./setup.sh
 
-source /workspaces/openai-cookbook/examples/vector_databases/redis/.venv/bin/activate
-
 echo "Starting uvicorn"
-uvicorn app:app --reload --host 0.0.0.0 --port 8000
+/home/ec2-user/.local/bin/uvicorn app:app --reload --host 0.0.0.0 --port 8000
