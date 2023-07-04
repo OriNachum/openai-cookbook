9e559ad7f9595fcab760d2174e54a3f9c15ab44b
Ori Nachum <ori.nachum@gmail.com>
2023-07-04 05:11:50 +0000

    fix reference

--- examples/vector_databases/redis/scripts/add_records.py
+++ examples/vector_databases/redis/scripts/add_records.py
@@ -1,6 +1,6 @@
 import redis
 
-from create_embedding import create_embedding
+from scripts.create_embedding import create_embedding
 from typing import List
 
 def get_next_vector_id(redis_client: redis.Redis) -> int:
