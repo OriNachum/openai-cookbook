9cc17ee7190cf81b121b004c331b4485ae0532cf
Ori Nachum <ori.nachum@gmail.com>
2023-07-04 18:34:49 +0000

    remove answer and date

--- examples/vector_databases/redis/scripts/convert_newrecordlist_to_csv.py
+++ examples/vector_databases/redis/scripts/convert_newrecordlist_to_csv.py
@@ -8,8 +8,8 @@ from scripts.generate_unique_public_id import generate_unique_public_id
 class NewRecord:
     def __init__(self, question: str, answer: str, date: date, company_name: str, company_size: str, company_location: str, company_industry: str):
         self.question = question
-        self.answer = answer
-        self.date = date
+        #self.answer = answer
+        #self.date = date
         self.company_name = company_name
         self.company_size = company_size
         self.company_location = company_location
@@ -21,7 +21,7 @@ def convert_newrecordlist_to_csv(records: List[NewRecord], filename: str):
 
     with open(filename, mode=mode, newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["id","question", "answer", "date", "company_name", "company_size", "company_location", "company_industry"])  # Write the header
+        writer.writerow(["id","question", "company_name", "company_size", "company_location", "company_industry"])  # Write the header
         for record in records:
             id=generate_unique_public_id()
-            writer.writerow([id, record.question, record.answer, record.date, record.company_name, record.company_size, record.company_location, record.company_industry])
+            writer.writerow([id, record.question, record.company_name, record.company_size, record.company_location, record.company_industry])
