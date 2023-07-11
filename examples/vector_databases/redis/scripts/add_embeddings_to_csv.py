381c188308e4e02dc67baed7319f53b638dc0360
Ori Nachum <ori.nachum@gmail.com>
2023-07-11 06:34:28 +0300

    Update add_embeddings_to_csv.py

--- examples/vector_databases/redis/scripts/add_embeddings_to_csv.py
+++ examples/vector_databases/redis/scripts/add_embeddings_to_csv.py
@@ -1,7 +1,27 @@
 import pandas as pd
 import csv
+import threading
+from queue import Queue
+import asyncio
+import time
 
 from scripts.create_embedding import create_embedding
+
+# Create a semaphore with a maximum of 10 concurrent threads
+semaphore = threading.Semaphore(10)
+
+def worker(q, results):
+    with semaphore:
+        while not q.empty():
+            item = q.get()
+            vector = create_embedding(item)
+            results.append(vector)
+            q.task_done()
+
+def process_item_with_threading(queue, results):
+    thread = threading.Thread(target=worker, args=(queue, results))
+    thread.start()
+    thread.join()
 
 def add_embeddings_to_csv(file_path: str):
     df = pd.read_csv(file_path)
@@ -16,11 +36,20 @@ def add_embeddings_to_csv(file_path: str):
         question = row['question']
         answer = row['answer']
 
-        question_vector = create_embedding(question)
-        answer_vector = create_embedding(answer)
+        question_queue = Queue()
+        answer_queue = Queue()
 
-        question_vectors.append(question_vector)
-        answer_vectors.append(answer_vector)
+        question_queue.put(question)
+        answer_queue.put(answer)
+
+        question_results = []
+        answer_results = []
+
+        process_item_with_threading(question_queue, question_results)
+        process_item_with_threading(answer_queue, answer_results)
+
+        question_vectors.append(question_results[0])
+        answer_vectors.append(answer_results[0])
         vector_ids.append(id_counter)
 
         id_counter += 1
