931e1813c85dff55f34fe8100c0c2efa50e2c803
Ori Nachum <ori.nachum@gmail.com>
2023-07-16 02:36:43 +0300

    Update create_embedding.py

--- examples/vector_databases/redis/scripts/create_embedding.py
+++ examples/vector_databases/redis/scripts/create_embedding.py
@@ -6,7 +6,7 @@ import time
 def create_embedding(user_query: str, max_retries: int = 3) -> list:
     # Creates embedding vector from user query
     system_prompt = "You are a data scientest master in classifiying fintec related content into relevant items. You will get a part of conversation or FAQ, and you will draw out of it a list of most significent fintech or business related terms. Do not explain any of them - only list them in a comma delimited format"
-    gpt_model = "gpt-4"
+    gpt_model = "gpt-3.5-turbo" #"gpt-4"
     manipulated_query = complete_prompt(user_query, system_prompt, gpt_model, 0.05)
     
     for i in range(max_retries):
