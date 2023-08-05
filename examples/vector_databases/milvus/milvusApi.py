e756d85213da28d48e90baa5f1500f9624c479e1
Ori Nachum <ori.nachum@gmail.com>
2023-08-05 20:34:21 +0300

    set milvusApi as debug for auto-reload on change

--- examples/vector_databases/milvus/milvusApi.py
+++ examples/vector_databases/milvus/milvusApi.py
@@ -63,4 +63,4 @@ def search_records():
     return jsonify({"results": results}), 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
